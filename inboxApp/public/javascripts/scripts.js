55772aac3bcf1db8a9b9666e5fbf6d8ae3f4fd7d
Lisa Carlson <lisa.carlson85@gmail.com>
2015-10-09 13:28:07 -0600

    user can select all messages

--- inboxApp/public/javascripts/scripts.js
+++ inboxApp/public/javascripts/scripts.js
@@ -1,3 +1,17 @@
 $( document ).ready(function() {
   $('button').prop('disabled', true);
+
+  $( "#multiselect" ).on( "click", function(){    
+    
+    $(".message").toggleClass('selected');
+    $('input[type=checkbox]').each(function() {
+      $(this).prop( "checked", true );
+      $('button').prop('disabled', false);
+    })
+  
+
+
+  });
+
 });
+
