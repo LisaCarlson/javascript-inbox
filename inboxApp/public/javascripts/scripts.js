382239b1e2d7a1c7e12ace82843c0a835a75a1f8
Lisa Carlson <lisa.carlson85@gmail.com>
2015-10-10 14:35:07 -0600

    User can star and unstar a message

--- inboxApp/public/javascripts/scripts.js
+++ inboxApp/public/javascripts/scripts.js
@@ -18,27 +18,40 @@ $( document ).ready(function() {
     }   
   });
 
-  $('input[type=checkbox]').on('click', function() {
-    $(this).closest('.message').toggleClass('selected');
-    $('button').prop('disabled', false); 
-    runCheck();   
+  $('input[type=checkbox]').on('click', function() { 
+    $(this).closest('.message').toggleClass('selected'); 
+    runCheck();  
   });
 
+  $(".fa").on('click', function() {
+    if ($(this).hasClass('fa-star-o')) {
+      $(this).removeClass('fa-star-o').addClass('fa-star');
+    }
+    else if ($(this).hasClass('fa-star')) {
+      $(this).removeClass('fa-star').addClass('fa-star-o');
+    }
+  });
 
 
 function runCheck() {
   var $checkedBoxes = $("input[type=checkbox]:checked");
   var $messageCheckboxes = $('.message-checkbox');
+  console.log($messageCheckboxes.length)
+  console.log($("input.message-checkbox[type=checkbox]:checked").length)
 
-$('input[type=checkbox]').each(function() {
     if ($checkedBoxes.length === $messageCheckboxes.length) {
       $( "#multiselect" ).prop("indeterminate", false).prop("checked", true);
-    } 
-    else if ($checkedBoxes.length < $messageCheckboxes.length) {
-      $( "#multiselect" ).prop("indeterminate", true);
+      $('button').prop('disabled', false);
     }
-     
-  });
+    else if ($checkedBoxes.length === 0) {
+      $( "#multiselect" ).prop("indeterminate", false);
+      $( "#multiselect" ).prop("checked", false);
+      $('button').prop('disabled', true);
+    } 
+    else if ($("input.message-checkbox[type=checkbox]:checked").length < $messageCheckboxes.length) {
+      $( "#multiselect" ).prop("indeterminate", true);
+      $('button').prop('disabled', false);
+    }        
 }
 
 
